f0d7ff04aae8a3ab1892c15eb17e4567b2988c9b
Pascal Nkornyui <86390213+0x15BA88FF@users.noreply.github.com>
2025-01-01 00:52:56 +0000

    Remove neovim configuration

--- hyproasis/nvim/.config/nvim/lua/configs/treesitter.lua
+++
@@ -1,8 +0,0 @@
-local options = {
-    ensure_installed = {
-        "vim", "lua", "vimdoc",
-        "html", "css", "javascript", "tsx", "typescript",
-    }
-}
-
-return options
--- hyproasis/nvim/.config/nvim/lua/mappings.lua
+++
@@ -1,6 +0,0 @@
-require "nvchad.mappings"
-
-local map = vim.keymap.set
-
-map("i", "jj", "<ESC>", { desc = "Quick escape" })
-map("n", ";", ":", { desc = "CMD enter command mode" })
